67d9c6fadb38565c7afff6a0b2747e297108e62e
paicaloid <paicaloid@gmail.com>
2022-09-05 13:19:45 +0700

    Change data to train and test

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -1,7 +1,8 @@
 class BaseTrain(object):
-    def __init__(self, model, data, config):
+    def __init__(self, model, train_dataset, validation_dataset, config):
         self.model = model
-        self.data = data
+        self.train_dataset = train_dataset
+        self.validation_dataset = validation_dataset
         self.config = config
 
     def train(self):
